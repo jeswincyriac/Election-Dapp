772942a1aedc3e5739d01cbcefc0b849a3f16b6f
Jeswin <jeswincyriac.k@gmail.com>
2018-08-25 11:17:12 +0530

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.24;
+
+contract Election{                   //like a class
+
+    string public candidate;         //declaring state variable
+    function Election ()  public{
+        candidate = "Candidate 1";
+    }
+
+}
